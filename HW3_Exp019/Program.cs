15f9b9406acb81420a96f3655647d5008028e818
Ann <anaykanaeva@yandex.ru>
2022-07-15 17:37:46 +0300

    HW3_Exp019

---
+++ HW3_Exp019/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+Console.WriteLine("Введите пятизначное число:");
+int number = int.Parse(Console.ReadLine());
+string str = number.ToString();
+
+ 
+while (str.Length <= 5)
+if (str[0]==str[4] && str[1]==str[3])
+{ 
+    Console.WriteLine ("Это палиндром");
+    break;
+}
+else  
+{
+    Console.WriteLine ("Это не палиндром");
+    break;
+}
+  
